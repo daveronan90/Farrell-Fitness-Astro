5190398d600150e0a5e765e13da8ebdf366a81eb
daveronan90 <daveronan90@gmail.com>
2023-06-18 21:26:40 +0100

    Add logo and setup for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), image()],
+  output: "server",
+  adapter: vercel()
 });
