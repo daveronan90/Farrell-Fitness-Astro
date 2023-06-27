8a73ca7279f472022e3087293ae02d9483b24bc2
daveronan90 <daveronan90@gmail.com>
2023-06-27 20:56:23 +0100

    Added two pages updated favicon and nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,7 +11,7 @@ export default function Nav() {
       <a className="relative z-50" href="/">
         <TextLogo />
       </a>
-      <div onClick={() => setToggled(!toggled)}>
+      <div onClick={() => setToggled(!toggled)} className="md:hidden">
         <div className="relative z-50 mr-2">
           <Hamburger toggled={toggled} toggle={setToggled} size={20} />
         </div>
@@ -22,12 +22,35 @@ export default function Nav() {
             className="fixed inset-0 z-40 flex h-screen w-full flex-col items-center justify-center bg-orange-600"
           >
             <div className="flex h-full flex-col items-center justify-evenly">
-              <a href="/">Home</a>
-              <a href="/pricing">Pricing</a>
-              <a href="/">Classes</a>
+              <a className="hover:text-orange-800" href="/">
+                Home
+              </a>
+              <a className="hover:text-orange-800" href="/pricing">
+                Membership
+              </a>
+              <a className="hover:text-orange-800" href="/classes">
+                Classes
+              </a>
+              <a className="hover:text-orange-800" href="/results">
+                results
+              </a>
             </div>
           </motion.div>
         )}
+      </div>
+      <div className="md:flex hidden w-full justify-end items-center text-xl relative z-20 space-x-6">
+        <a className="hover:text-orange-800" href="/">
+          home
+        </a>
+        <a className="hover:text-orange-800" href="/pricing">
+          membership
+        </a>
+        <a className="hover:text-orange-800" href="/classes">
+          classes
+        </a>
+        <a className="hover:text-orange-800" href="/results">
+          results
+        </a>
       </div>
     </nav>
   );
