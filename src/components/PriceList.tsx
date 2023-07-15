071eb0f9c4497f119b881ee0bdd3622ddd15c7a7
daveronan90 <daveronan90@gmail.com>
2023-07-15 15:58:45 +0100

    Update pricing information

--- src/components/PriceList.tsx
+++ src/components/PriceList.tsx
@@ -75,16 +75,14 @@ export default function PriceList() {
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.5, delay: sIndex * 0.05 }}
                 key={sIndex}
-                className="w-full flex"
+                className="flex md:ml-4 ml-2 font-normal"
               >
-                <div className="w-1/2 px-4 py-3 font-normal">{desc}</div>
-                <div className="flex w-1/2 text-end">
-                  <div className="w-2/3 px-4 py-3 font-normal text-right">
-                    {duration !== "null" && duration}
-                  </div>
-                  <div className="w-1/3 px-4 py-3 font-normal">
-                    {`${price != "0" ? `€${price}` : ""}`}
-                  </div>
+                <div className="py-3">{desc}</div>
+                <div className="flex-grow py-3 px-2 md:px-12">
+                  {duration !== "null" && duration}
+                </div>
+                <div className="py-3">
+                  {`${price != "0" ? `€${price}` : ""}`}
                 </div>
               </motion.div>
             ))}
