4ed420500001fede97c55cf0985177afeb3ad84d
daveronan90 <daveronan90@gmail.com>
2023-07-15 12:32:02 +0100

    Fixed the ordering for the price list

--- src/components/PriceList.tsx
+++ src/components/PriceList.tsx
@@ -1,10 +1,70 @@
-import { defaultPriceList } from "../js/pricingData";
+import { fetchData } from "../js/airtable";
+import { ServiceGroup, defaultPriceList } from "../js/pricingData";
+import { useState, useEffect } from "react";
+
+interface RawPricingData {
+  category: string;
+  desc: string;
+  duration: string;
+  price: string;
+  order: number;
+}
+
+interface EmptyObject {
+  [key: string]: any;
+}
+
+function groupByCategory(array: RawPricingData[]) {
+  const grouped: EmptyObject | ServiceGroup = {};
+
+  for (const item of array) {
+    const { category, desc, duration, price, order } = item;
+
+    if (!grouped[category]) {
+      grouped[category] = {
+        category,
+        services: [],
+        order,
+      };
+    }
+
+    grouped[category].services.push({
+      desc,
+      duration,
+      price,
+      order,
+    });
+  }
+  return Object.values(grouped);
+}
 
 export default function PriceList() {
+  const [priceList, setPriceList] = useState(defaultPriceList);
+
+  const getData = async () => {
+    const data: RawPricingData[] = await fetchData("PriceList");
+
+    const groupedData: ServiceGroup[] = groupByCategory(data);
+
+    const sortedGroupData = groupedData.sort((a, b) => {
+      a.services.sort((c, d) => c.order - d.order);
+      b.services.sort((e, f) => e.order - f.order);
+      return a.order - b.order;
+    });
+
+    console.log(sortedGroupData);
+
+    setPriceList(sortedGroupData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="text-xxs md:text-base">
       <div className="font-light">
-        {defaultPriceList.map(({ category, services }, index) => (
+        {priceList.map(({ category, services }, index) => (
           <div key={index} className="w-full">
             <div>
               <div className="py-4 text-base md:text-2xl font-medium">
--- src/js/pricingData.ts
+++ src/js/pricingData.ts
@@ -2,92 +2,130 @@ export interface service {
   desc: string;
   duration: string;
   price: string;
+  order: number;
 }
 
 export interface ServiceGroup {
   category: string;
   services: service[];
+  order: number;
 }
 
 export const defaultPriceList: ServiceGroup[] = [
   {
     category: "classes",
+    order: 1,
     services: [
-      { desc: "8 classes", duration: "1 month", price: "TBD" },
-      { desc: "12 classes", duration: "1 month", price: "TBD" },
-      { desc: "classes & gym", duration: "1 month", price: "TBD" },
-      { desc: "class", duration: "1 day", price: "TBD" },
+      { desc: "8 classes", duration: "1 month", price: "TBD", order: 1 },
+      { desc: "12 classes", duration: "1 month", price: "TBD", order: 2 },
+      { desc: "classes & gym", duration: "1 month", price: "TBD", order: 3 },
+      { desc: "class", duration: "1 day", price: "TBD", order: 4 },
     ],
   },
   {
     category: "open gym",
+    order: 2,
     services: [
-      { desc: "gym", duration: "1 month", price: "TBD" },
-      { desc: "gym", duration: "3 months", price: "TBD" },
-      { desc: "gym", duration: "6 months", price: "TBD" },
-      { desc: "gym", duration: "1 year", price: "TBD" },
-      { desc: "gym", duration: "1 day", price: "TBD" },
-      { desc: "gym", duration: "1 week", price: "TBD" },
-      { desc: "gym (student)", duration: "1 month", price: "TBD" },
-      { desc: "gym (student)", duration: "3 months", price: "TBD" },
-      { desc: "gym (student)", duration: "6 months", price: "TBD" },
-      { desc: "gym (student)", duration: "1 year", price: "TBD" },
+      { desc: "gym", duration: "1 month", price: "TBD", order: 5 },
+      { desc: "gym", duration: "3 months", price: "TBD", order: 6 },
+      { desc: "gym", duration: "6 months", price: "TBD", order: 7 },
+      { desc: "gym", duration: "1 year", price: "TBD", order: 8 },
+      { desc: "gym", duration: "1 day", price: "TBD", order: 9 },
+      { desc: "gym", duration: "1 week", price: "TBD", order: 10 },
+      { desc: "gym (student)", duration: "1 month", price: "TBD", order: 11 },
+      { desc: "gym (student)", duration: "3 months", price: "TBD", order: 12 },
+      { desc: "gym (student)", duration: "6 months", price: "TBD", order: 13 },
+      { desc: "gym (student)", duration: "1 year", price: "TBD", order: 14 },
     ],
   },
   {
     category: "personal training",
+    order: 3,
     services: [
-      { desc: "personal training", duration: "1 session", price: "TBD" },
+      {
+        desc: "personal training",
+        duration: "1 session",
+        price: "TBD",
+        order: 15,
+      },
       {
         desc: "personal training 1 week",
         duration: "2 sessions",
         price: "TBD",
+        order: 16,
       },
       {
         desc: "personal training 5 week block",
         duration: "10 sessions",
         price: "TBD",
+        order: 17,
       },
       {
         desc: "semi personal training 2 people",
         duration: "1 session",
         price: "TBD",
+        order: 18,
       },
     ],
   },
   {
     category: "gym & programming",
+    order: 4,
     services: [
-      { desc: "gym & program", duration: "1 month", price: "TBD" },
-      { desc: "gym & program", duration: "3 months", price: "TBD" },
+      { desc: "gym & program", duration: "1 month", price: "TBD", order: 19 },
+      { desc: "gym & program", duration: "3 months", price: "TBD", order: 20 },
       {
         desc: "consulation",
         duration: "body scan mobility screen strength test",
         price: "TBD",
+        order: 4,
       },
     ],
   },
   {
     category: "sports message",
+    order: 5,
     services: [
-      { desc: "sports massage (non member)", duration: "", price: "TBD" },
-      { desc: "sports massage (member)", duration: "", price: "TBD" },
+      {
+        desc: "sports massage (non member)",
+        duration: "",
+        price: "TBD",
+        order: 21,
+      },
+      {
+        desc: "sports massage (member)",
+        duration: "",
+        price: "TBD",
+        order: 22,
+      },
     ],
   },
   {
     category: "styku 3d body scan",
+    order: 6,
     services: [
-      { desc: "styku body scan", duration: "", price: "TBD" },
-      { desc: "styku body scan (student)", duration: "", price: "TBD" },
+      { desc: "styku body scan", duration: "", price: "TBD", order: 23 },
+      {
+        desc: "styku body scan (student)",
+        duration: "",
+        price: "TBD",
+        order: 24,
+      },
     ],
   },
   {
     category: "pt & gym program",
+    order: 7,
     services: [
-      { desc: "personal training", duration: "5 sessions", price: "TBD" },
-      { desc: "gym", duration: "3 months", price: "TBD" },
-      { desc: "program", duration: "3 months", price: "TBD" },
-      { desc: "", duration: "Total", price: "TBD" },
+      {
+        desc: "personal training",
+        duration: "5 sessions",
+        price: "TBD",
+        order: 25,
+      },
+      { desc: "gym", duration: "3 months", price: "TBD", order: 26 },
+      { desc: "program", duration: "3 months", price: "TBD", order: 27 },
+      { desc: "", duration: "Total", price: "TBD", order: 28 },
     ],
   },
 ];
